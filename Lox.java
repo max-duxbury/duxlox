012e3a81ec5d85d6f126ce4e959ead1babcfc77e
max-duxbury <max.duxbury@outlook.com>
2026-03-16 20:42:54 +0000

    Initial Commit & Code from Ch.4 (Scanning) & Challenges TODO

---
+++ Lox.java
@@ -0,0 +1,74 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+  static boolean hadError = false;
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64); 
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+ }
+}
+
+
+// Handles jlox interpretation when path to file is given
+private static void runFile(String path) throws IOException {
+	byte[] bytes = Files.readAllBytes(Path.get(path));
+	run(new String(bytes, Charset.defaultCharset()));
+  
+  //Indicates error in the exit code
+  if (hadError) System.exit(65);
+}
+
+// Handles interpretation when no path/arguments are given
+private static void runPrompt() throws IOException {
+	InputStreamReader input = new InputStreamReader(System.in);
+	BufferedReader reader = new BufferedReader(input);
+
+  	for (;;) { 
+    		System.out.print("> ");
+    		String line = reader.readLine();
+    		if (line == null) break;
+    		run(line);
+        hadError = false;
+  	}
+
+}
+
+// Core function for the interpretor
+private static void run(String source) {
+	Scanner scanner = new Scanner (source);
+	List<token> tokens = scanner.scanTokens();
+
+	// Just print the tokens for now
+	for (Token token : tokens) {
+		System.out.println(token);
+	}
+}
+
+// Error Handling - just line no.
+// Book mentions further implementation i.e error handling that gives you better messages
+// i.e what and where the error is
+static void error(int line, String message) {
+  report(line, "", message);
+}
+
+private static void report(int line, String where, String message) {
+  System.err.println(
+    "[line " + line + "] Error" + where + ": " + message);
+  hadError = true;
+  )
+}
+
